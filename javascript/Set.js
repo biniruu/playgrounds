4c3fdc1302deaf52a6b7db8cae03781a7ca94a61
biniruu <biniruu@gmail.com>
2022-10-13 23:51:15 +0900

    add Set.js

---
+++ javascript/Set.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+/**
+ * 학습 자료
+ * 맵과 셋 : https://ko.javascript.info/map-set
+ * Object.entries() : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+ * Object.fromEntries() : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
+ */
