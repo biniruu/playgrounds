24394be1c42f867518c5985e9c28e3814f912cb8
biniruu <biniruu@gmail.com>
2022-10-25 23:23:03 +0900

    update Set.js

--- javascript/Set.js
+++ javascript/Set.js
@@ -20,56 +20,79 @@
 // es5까지는 Object, Array만
 // es6부터 Map, Set, WeakMap, WeakSet, Typed Array 추가
 
-// 주요 메서드
+/**
+ * 주요 메서드
+ */
 
-// new Set(iterable) : iterable 객체를 전달 받으면(대체로 배열을 받음) 그 값을 복사해서 셋에 추가한다
-const set = new Set([1, 'a', true])
-console.log(set) // Set(3) { 1, 'a', true }
+// 1️⃣ new Set(iterable) : iterable 객체를 전달 받으면(대체로 배열을 받음) 그 값을 복사해서 셋에 추가한다
+{
+  const set = new Set([1, 'a', true])
+  console.log(set) // Set(3) { 1, 'a', true }
 
-// iterable 객체가 아닌 것을 전달 받으면 어떻게 되지?
-// const obj = { apple: '🍎', banana: '🍌' }
-// const setObj = new Set(obj) // 에러 발생
+  // iterable 객체가 아닌 것을 전달 받으면 어떻게 되지?
+  // const obj = { apple: '🍎', banana: '🍌' }
+  // const setObj = new Set(obj) // 에러 발생
 
-// 값을 복사해서 셋에 추가한다면 참조가 아닌 값 자체를 복사하나?
-// 그렇다. 일단은
-const arr = ['🍎', '🍌']
-const setArr = new Set(arr)
-console.log(setArr) // Set(2) { '🍎', '🍌' }
+  // 값을 복사해서 셋에 추가한다면 참조가 아닌 값 자체를 복사하나?
+  // 그렇다. 일단은
+  const arr = ['🍎', '🍌']
+  const setArr = new Set(arr)
+  console.log(setArr) // Set(2) { '🍎', '🍌' }
 
-arr[1] = '🥕'
-console.log(arr) // [ '🍎', '🥕' ]
-console.log(setArr) // Set(2) { '🍎', '🍌' }
+  arr[1] = '🥕'
+  console.log(arr) // [ '🍎', '🥕' ]
+  console.log(setArr) // Set(2) { '🍎', '🍌' }
 
-// depth가 깊은 iterable 객체도 모두 복사하나?
-// 셋은 얕은 복사를 한다. 직접 비교해 보자
+  // depth가 깊은 iterable 객체도 모두 복사하나?
+  // 셋은 얕은 복사를 한다. 직접 비교해 보자
 
-// 비교 대상이 되는 배열
-const arrDeep = ['🍎', ['🥕', ['🚀']]]
+  // 비교 대상이 되는 배열
+  const arrDeep = ['🍎', ['🥕', ['🚀']]]
 
-// 얕은 복사를 이용해 arrDeep을 새로운 배열로 생성
-const arrDeepClone = [...arrDeep]
-console.log(arrDeepClone) // [ '🍎', [ '🥕', [ '🚀' ] ] ]
+  // 얕은 복사를 이용해 arrDeep을 새로운 배열로 생성
+  const arrDeepClone = [...arrDeep]
+  console.log(arrDeepClone) // [ '🍎', [ '🥕', [ '🚀' ] ] ]
 
-// Set 생성
-const setArrDeep = new Set(arrDeep)
-console.log(setArrDeep) // Set(2) { '🍎', [ '🥕', [ '🚀' ] ] }
+  // Set 생성
+  const setArrDeep = new Set(arrDeep)
+  console.log(setArrDeep) // Set(2) { '🍎', [ '🥕', [ '🚀' ] ] }
 
-// 깊은 복사를 이용해 arrDeep을 새로운 배열로 생성
-const trueDeepClone = JSON.parse(JSON.stringify(arrDeep))
-console.log(trueDeepClone) // [ '🍎', [ '🥕', [ '🚀' ] ] ]
+  // 깊은 복사를 이용해 arrDeep을 새로운 배열로 생성
+  const trueDeepClone = JSON.parse(JSON.stringify(arrDeep))
+  console.log(trueDeepClone) // [ '🍎', [ '🥕', [ '🚀' ] ] ]
 
-// 데이터 변경 시도
-arrDeep[0] = '🎊'
-arrDeep[1][1] = '🔥'
+  // 데이터 변경 시도
+  arrDeep[0] = '🎊'
+  arrDeep[1][1] = '🔥'
 
-// arrDeep은 당연히 변경됨
-console.log(arrDeep) // [ '🎊', [ '🥕', '🔥' ] ]
+  // arrDeep은 당연히 변경됨
+  console.log(arrDeep) // [ '🎊', [ '🥕', '🔥' ] ]
 
-// arrDeep의 얕은 복사는 arrDeep[0]만 변경되지 않고 다른 것은 변경됨
-console.log(arrDeepClone) // [ '🍎', [ '🥕', '🔥' ] ]
+  // arrDeep의 얕은 복사는 arrDeep[0]만 변경되지 않고 다른 것은 변경됨
+  console.log(arrDeepClone) // [ '🍎', [ '🥕', '🔥' ] ]
 
-// Set도 위와 동일
-console.log(setArrDeep) // Set(2) { '🍎', [ '🥕', '🔥' ] }
+  // Set도 위와 동일
+  console.log(setArrDeep) // Set(2) { '🍎', [ '🥕', '🔥' ] }
 
-// arrDeep의 깊은 복사만 아무 것도 변경되지 않음
-console.log(trueDeepClone) // [ '🍎', [ '🥕', [ '🚀' ] ] ]
+  // arrDeep의 깊은 복사만 아무 것도 변경되지 않음
+  console.log(trueDeepClone) // [ '🍎', [ '🥕', [ '🚀' ] ] ]
+}
+
+// 2️⃣ add(value)
+{
+  const set = new Set()
+
+  // 값을 추가해 보자
+  const myName = { name: 'biniruu' }
+  set.add(myName)
+
+  // 값이 추가되었다
+  console.log(set) // Set(1) { { name: 'biniruu' } }
+
+  // 다른 값도 추가해보자
+  const favouriteFood = { food: '🍕' }
+  set.add(favouriteFood)
+
+  // 역시 값이 추가되었다
+  console.log(set) // Set(2) { { name: 'biniruu' }, { food: '🍕' } }
+}
