70a2859b85d343114760ee38e6d5228606f2e0e7
biniruu <biniruu@gmail.com>
2022-11-14 21:39:58 +0900

    update some settings

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,21 +1,10 @@
 import '_css/style.css'
 import '_/App.css'
 
-import Temp from 'components/temp/Temp'
-
-// import React from 'react'
-
 function App() {
-  const promise = new Promise(resolve => {
-    resolve('🚀')
-  })
-  const result = await promise
-  console.log(result)
-
   return (
     <div>
       <p className="bold App">Lorem ipsum dolor sit</p>
-      <Temp />
     </div>
   )
 }
