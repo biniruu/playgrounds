0a21089cf0ae2b744817a716153c1a297baefd70
biniruu <biniruu@gmail.com>
2023-01-28 19:37:52 +0900

    feat: add redux to react app

--- react-typescript/src/index.tsx
+++ react-typescript/src/index.tsx
@@ -1,15 +1,19 @@
 import 'index.css'
 
 import App from 'App'
+import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from 'reportWebVitals'
+import store from 'store/store'
 
 // eslint-disable-next-line prettier/prettier
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
 
