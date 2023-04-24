ba7232d52c79e73166926600a5f194182ff90503
biniruu <biniruu@gmail.com>
2023-04-25 01:05:44 +0900

    feat: add react router to index.tsx

--- rooms/learning-react-typescript/src/index.tsx
+++ rooms/learning-react-typescript/src/index.tsx
@@ -1,18 +1,26 @@
 import 'index.css'
 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
 import App from 'App'
-import { Provider } from 'react-redux'
+import store from 'app/store'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import reportWebVitals from 'reportWebVitals'
-import store from 'app/store'
+import UseState from './pages/UseState'
 
 // eslint-disable-next-line prettier/prettier
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/state" element={<UseState />} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>,
 )
