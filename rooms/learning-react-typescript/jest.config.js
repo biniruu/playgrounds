ab6cfdd1c97612d75db824a0352fce14ef01e707
biniruu <biniruu@gmail.com>
2023-05-13 21:45:17 +0900

    chore: remove unnecessary files

--- rooms/learning-react-typescript/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  moduleNameMapper: {
-    '^\\$components/(.*)$': '<rootDir>/src/components/$1',
-    '^\\$css/(.*)$': '<rootDir>/src/assets/css/$1',
-  },
-}
