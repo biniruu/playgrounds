8f1ede6fc758cde66703f0f15a2dac7c3097348e
biniruu <biniruu@gmail.com>
2023-05-15 16:26:12 +0900

    feat: implement 2nd example feature of useContext hook

--- rooms/learning-react-typescript/src/pages/UseReducer.tsx
+++ rooms/learning-react-typescript/src/pages/UseReducer.tsx
@@ -1,9 +1,12 @@
 import FirstComponent from 'components/use-reducer/FirstComponent'
+import SecondComponent from 'components/use-reducer/SecondComponent'
 
 function UseReducer() {
   return (
     <div>
+      <h1 className="mb-4 text-xl">UseReducer</h1>
       <FirstComponent />
+      <SecondComponent />
     </div>
   )
 }
