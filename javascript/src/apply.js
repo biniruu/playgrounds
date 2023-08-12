261e16fa56513081f5e77a3e9edbe92d50e29de7
biniruu <biniruu@gmail.com>
2023-08-12 11:55:46 +0900

    feat: apply.js

---
+++ javascript/src/apply.js
@@ -0,0 +1,20 @@
+const num = [2, 5, 4]
+
+const func = (num1, num2, num3) => `num1 = ${num1}, num2 = ${num2}, num3 = ${num3}`
+const result1 = func.apply(this, num)
+
+console.log(result1) // num1 = 2, num2 = 5, num3 = 4
+
+/* unrelated to apply */
+{
+  const arrParamFunc = ([num1, num2, num3]) => `num1 = ${num1}, num2 = ${num2}, num3 = ${num3}`
+  const result2 = arrParamFunc(num)
+
+  console.log(result2) // num1 = 2, num2 = 5, num3 = 4
+
+  const wrong1 = func([...num])
+  const wrong2 = func(num)
+
+  console.log(wrong1) // num1 = 2,5,4, num2 = undefined, num3 = undefined
+  console.log(wrong2) // num1 = 2,5,4, num2 = undefined, num3 = undefined
+}
