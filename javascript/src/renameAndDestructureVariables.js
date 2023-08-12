b62da6dc13030d2da2370fbb4188b79aceeba0ae
biniruu <biniruu@gmail.com>
2023-08-12 19:00:23 +0900

    feat: add renameAndDestructureVariables

---
+++ javascript/src/renameAndDestructureVariables.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+const profile = {
+  name: 'Mars',
+  age: 9999_9999_9999,
+  address: 'Universe',
+}
+
+const { name: myName, age: myAge, address: myAddress } = profile
+
+console.log(myName, myAge, myAddress) // Mars 999999999999, Universe
