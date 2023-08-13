c34c077ed6f1421693c451ae4adac054ea8371b0
biniruu <biniruu@gmail.com>
2023-08-14 01:46:02 +0900

    feat: move business logic from NewsList.tsx to other components

--- react/swr/src/components/MultipleArguments.tsx
+++ react/swr/src/components/MultipleArguments.tsx
@@ -1,18 +1,35 @@
+import { useLayoutEffect, useState } from 'react'
 import useSWR from 'swr'
 
-import { News } from '../types/news.d'
+import { News } from '../types/news'
 
 import NewsList from './NewsList'
+
+const initialStateOfNews = [
+  {
+    thumb: '',
+    name: '',
+    url: '',
+  },
+]
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const fetchWithToken = (url: string, token: string) => fetch(url).then(res => res.json())
 
 function MultipleArguments() {
+  const [news, setNews] = useState(initialStateOfNews)
+
   const { data } = useSWR<News>(['/channel/rightbar?officeId=296', '🍎'], ([url, token]: [string, string]) =>
     fetchWithToken(url, token),
   )
 
-  return <NewsList heading="MultipleArguments" data={data} />
+  useLayoutEffect(() => {
+    if (data?.result) {
+      setNews(data?.result.premiumContent.list)
+    }
+  }, [data])
+
+  return <NewsList heading="MultipleArguments" news={news} />
 }
 
 export default MultipleArguments
