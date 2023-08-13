60e2dc57547a7e6feea502e39d9e62445f72c6a9
biniruu <biniruu@gmail.com>
2023-08-14 01:41:00 +0900

    refactor: move business logic from NewsList.tsx to Pagenation.tsx

--- react/swr/src/components/Pagenation.tsx
+++ react/swr/src/components/Pagenation.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useLayoutEffect, useState } from 'react'
 import useSWR from 'swr'
 
 import type { User } from '../types/user'
@@ -9,14 +9,22 @@ const fetcher = async (url: string) => fetch(url).then(response => response.json
 
 function Pagenation() {
   const [pageIdx, setPageIdx] = useState(2)
+  const [user, setUser] = useState('')
+  const [nextUser, setNextUser] = useState('')
+
   const { data } = useSWR<User>(`https://reqres.in/api/users/${pageIdx}`, fetcher)
   const { data: nextPage } = useSWR<User>(`https://reqres.in/api/users/${pageIdx + 1}`, fetcher)
 
+  useLayoutEffect(() => {
+    setUser(data?.data.email as string)
+    setNextUser(nextPage?.data.email as string)
+  }, [data, nextPage])
+
   return (
     <>
-      <NewsList heading="Pagenation" data={data} />
+      <NewsList heading="Pagenation" user={user} />
       <div className="hidden">
-        <NewsList heading="Pagenation" data={nextPage} />
+        <NewsList heading="Pagenation" user={nextUser} />
       </div>
       <div className="flex justify-center mt-4">
         <button type="button" className="px-4" onClick={() => setPageIdx(pageIdx - 1)}>
