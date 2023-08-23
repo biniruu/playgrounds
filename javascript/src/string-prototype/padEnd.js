3ca3d9d90914474c7ae649095a48acc8630fdf9c
biniruu <biniruu@gmail.com>
2023-08-23 17:30:30 +0900

    feat: add padEnd.js

---
+++ javascript/src/string-prototype/padEnd.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+/**
+ * String.prototype.padEnd()
+ * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padEnd}
+ */
+
+const username = 'biniruu'
+const first2Str = username.slice(0, 2)
+const maskUsername = first2Str.padEnd(username.length, '*')
+console.log('💃🏻 🕺🏻 maskUsername:', maskUsername)
