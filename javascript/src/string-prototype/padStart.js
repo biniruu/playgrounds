04c1063fdae4f26e031781f64ae9217d28812487
biniruu <biniruu@gmail.com>
2023-08-24 16:11:22 +0900

    feat: add padStart.js

---
+++ javascript/src/string-prototype/padStart.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+/**
+ * String.prototype.padStart()
+ * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart}
+ */
+
+const phoneNumber = '010-1234-5678'
+const phoneNumberWithoutDash = phoneNumber.replaceAll('-', '')
+const last4Digits = phoneNumberWithoutDash.slice(-4)
+const maskPhoneNumber = last4Digits.padStart(phoneNumberWithoutDash.length, '*')
+console.log('💃🏻 🕺🏻 maskPhoneNumber:', maskPhoneNumber)
