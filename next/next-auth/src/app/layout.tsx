f88c5656a33a1149355d99a37cc509f66a49a7fc
biniruu <biniruu@gmail.com>
2023-08-24 17:55:06 +0900

    chore: add SessionProvider

--- next/next-auth/src/app/layout.tsx
+++ next/next-auth/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import type { ReactNode } from 'react'
+
 import './globals.css'
+import Template from './template'
 
 // eslint-disable-next-line new-cap
 const inter = Inter({ subsets: ['latin'] })
@@ -14,7 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Template key="routeTemplate">{children}</Template>
+      </body>
     </html>
   )
 }
