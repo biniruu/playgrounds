54064a9cabee92a1e34b544ba90da3f1a2ab3c6d
biniruu <biniruu@gmail.com>
2023-08-28 18:39:07 +0900

    feat: implement spin-and-delete

---
+++ css/src/spin-and-delete-in-keyframes/index.js
@@ -0,0 +1,4 @@
+document.querySelector('.delete-btn').addEventListener('click', () => {
+  document.querySelector('.delete-btn').classList.add('fade-out')
+  document.querySelector('.card').classList.add('spin-out')
+})
