d68b0032ff7d32caa1a6ae6215333b29cce466ba
biniruu <biniruu@gmail.com>
2023-10-01 16:47:52 +0900

    chore: implement tailwind css with postcss and webpack

--- three/three-typescript/webpack.common.js
+++ three/three-typescript/webpack.common.js
@@ -1,13 +1,18 @@
 const path = require('path')
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/client/client.ts'),
+  entry: path.resolve(__dirname, 'src/client/client.ts'),
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.tsx?$/,
         use: 'ts-loader',
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
