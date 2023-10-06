b4c9d5ba8f54558ebb34a0065897a4e19f7d7626
biniruu <biniruu@gmail.com>
2023-10-07 01:55:48 +0900

    feat: add login path to router

--- react/react-router/src/router.js
+++ react/react-router/src/router.js
@@ -4,11 +4,13 @@ import RootLayout from './layouts/RootLayout'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
     </Route>,
