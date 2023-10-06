14b2ce85e3aebf6a10b2a71196d1bbb69ab88eeb
biniruu <biniruu@gmail.com>
2023-10-07 00:45:29 +0900

    feat: implement login authentication

--- react/react-router/src/layouts/RootLayout.jsx
+++ react/react-router/src/layouts/RootLayout.jsx
@@ -1,9 +1,20 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 function RootLayout() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('/auth')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [navigate])
+
   return (
     <>
       <Header />
