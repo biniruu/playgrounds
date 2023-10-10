0962430f2e94d0c5e5584012e0792f5d266ff332
biniruu <biniruu@gmail.com>
2023-10-10 17:41:00 +0900

    chore: install dotenv-webpack and dotenv

--- firebase/authentication/webpack.common.js
+++ firebase/authentication/webpack.common.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
 
+const Dotenv = require('dotenv-webpack')
+
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.ts'),
   module: {
@@ -16,6 +18,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new Dotenv()],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
