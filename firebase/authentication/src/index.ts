b3f2be9df723b9dda308b6f3c19ae39de8377526
biniruu <biniruu@gmail.com>
2023-10-11 02:01:17 +0900

    feat: implememt notice which subscribe status

--- firebase/authentication/src/index.ts
+++ firebase/authentication/src/index.ts
@@ -5,10 +5,11 @@ import { auth } from './utils/firebaseAuth'
 const emailForm = document.getElementById('email-auth') as HTMLFormElement
 const emailInput = document.getElementsByName('email')[0] as HTMLInputElement
 const pwInput = document.getElementsByName('password')[0] as HTMLInputElement
+const subscribeStatusElem = document.getElementById('subscribe-status') as HTMLParagraphElement
 
 const emailAuth = async (email: string, pw: string) => {
   const userCredential = await createUserWithEmailAndPassword(auth, email, pw)
-  console.log('💃🏻 🕺🏻 userCredential:', userCredential)
+  subscribeStatusElem.textContent = `${userCredential.user.email} subscribed successfully`
 }
 
 emailForm.addEventListener('submit', (e: SubmitEvent) => {
