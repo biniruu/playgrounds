8dbb519f837369fcb76c5ab3ec7f7c65cd0a8b03
biniruu <biniruu@gmail.com>
2023-10-18 20:15:44 +0900

    feat: add RecoilRoot to index.tsx

--- react/recoil/src/index.tsx
+++ react/recoil/src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { RecoilRoot } from 'recoil'
 import App from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RecoilRoot>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RecoilRoot>,
 )
 
 // If you want to start measuring performance in your app, pass a function
