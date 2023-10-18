09c342e1e06b2babfb8f9acb9f45a4e0262a5b90
biniruu <biniruu@gmail.com>
2023-10-19 05:15:28 +0900

    feat: add withPrefix to SelectorGet.tsx

--- react/recoil/src/components/SelectorGet.tsx
+++ react/recoil/src/components/SelectorGet.tsx
@@ -1,13 +1,21 @@
 import { useRecoilValue } from 'recoil'
-import { withSuffix } from 'recoil/text'
+import { withPrefix, withSuffix } from 'recoil/text'
 
 function SelectorGet() {
   const textWithSuffix = useRecoilValue(withSuffix)
+  const textWithPrefix = useRecoilValue(withPrefix)
 
   return (
     <div>
       <h1>SelectorGet</h1>
-      <p>{textWithSuffix}</p>
+      <dl>
+        <dt>text with suffix</dt>
+        <dd>{textWithSuffix}</dd>
+      </dl>
+      <dl>
+        <dt>text with prefix</dt>
+        <dd>{textWithPrefix}</dd>
+      </dl>
     </div>
   )
 }
