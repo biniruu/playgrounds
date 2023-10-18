04b5da8e73c0376f506a7918d86d2e78efceb93d
biniruu <biniruu@gmail.com>
2023-10-18 20:45:51 +0900

    feat: add text input

--- react/recoil/src/components/AtomSet.tsx
+++ react/recoil/src/components/AtomSet.tsx
@@ -1,8 +1,21 @@
+import { ChangeEvent } from 'react'
+import { useSetRecoilState } from 'recoil'
+import atomTestAtom from 'recoil/atomTest'
+
 function AtomSet() {
+  const setAtomTest = useSetRecoilState(atomTestAtom)
+
+  const handleChange = (e: ChangeEvent) => {
+    const target = e.target as typeof e.target & { text: { value: string } }
+    const value = target.text.value
+    setAtomTest(value)
+  }
+
   return (
-    <div>
+    <>
       <h1>AtomSet</h1>
-    </div>
+      <input type="text" name="text" id="text" onChange={handleChange} />
+    </>
   )
 }
 
