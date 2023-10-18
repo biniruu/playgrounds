2eea144c1cdc3db7a70f07040550e0f0d3fd48a6
biniruu <biniruu@gmail.com>
2023-10-18 20:29:44 +0900

    feat: add AtomSet.tsx and AtomGet.tsx to App.tsx

--- react/recoil/src/App.tsx
+++ react/recoil/src/App.tsx
@@ -1,7 +1,14 @@
+import AtomGet from 'components/AtomGet'
+import AtomSet from 'components/AtomSet'
 import './App.css'
 
 function App() {
-  return <div></div>
+  return (
+    <>
+      <AtomSet></AtomSet>
+      <AtomGet></AtomGet>
+    </>
+  )
 }
 
 export default App
