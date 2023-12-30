9bdadafdda826747efcc58806dc4914e2375cf31
biniruu <biniruu@gmail.com>
2023-12-30 16:03:57 +0900

    chore: create number.py in python/basic

---
+++ python/basic/number.py
@@ -0,0 +1,24 @@
+# nonzero numbers are considered True
+print(bool(True))
+print(bool(1))
+print(bool(15))
+print(bool(-1))
+print(bool(-200))
+
+# zero-valued ones are considered False
+print(bool(False))
+print(bool(0))
+print(bool(0.0))
+
+# remainder
+print(9 % 5)
+
+# floating-point division
+print(9 / 5)
+
+# integer division
+print(9 // 5)
+
+# exponents
+
+print(-5 ** 2)
