e62585e74d241af24ea9d48908ea436ee962ee06
biniruu <biniruu@gmail.com>
2023-12-30 17:13:40 +0900

    chore: create if.py

---
+++ python/basic/if.py
@@ -0,0 +1,22 @@
+sum = 1 + \
+    2 + \
+    3 + \
+    4
+
+print(sum)
+
+disaster = True
+
+if disaster:
+    print('Woe!')
+else:
+    print('Whee!')
+
+color = 'mauve'
+
+if color == 'red':
+    print("It's a tomato")
+elif color == 'mauve':
+    print("I've never heard of the color", color)
+else:
+    print('nothing')
