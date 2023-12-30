d654cbc95cafdde4795056d0ea659fd724327fa6
biniruu <biniruu@gmail.com>
2023-12-30 14:36:26 +0900

    init python/basic

---
+++ python/basic/variable.py
@@ -0,0 +1,17 @@
+help("keywords")
+
+x = 5
+y = x + 12
+print(y)
+
+print(type(7))
+print(isinstance(7, int))
+
+two = duex = zwei = 2
+print(two)
+print(duex)
+print(zwei)
+
+[x, y] = [y, x]
+print(x)
+print(y)
