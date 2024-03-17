7fc8bda3f792c4a3ee827aa348679dd1988862b9
biniruu <biniruu@gmail.com>
2024-03-17 23:30:19 +0900

    test: create productService.test.ts

---
+++ libraries/jest/src/mock/product/test/productService.test.ts
@@ -0,0 +1,21 @@
+import { ProductService } from '@mock/product/productService'
+import { StubProductClient } from '@mock/product/stubProductClient'
+
+describe('stub product client', () => {
+  let productService: ProductService<StubProductClient>
+
+  beforeEach(() => {
+    productService = new ProductService<StubProductClient>(new StubProductClient())
+  })
+
+  test('should fetch available items succesfully when productClient.fetchItems() returns available items', async () => {
+    const result = await productService.fetchAvailableItems()
+    const expected = [
+      { id: 1, name: 'Item 1', available: true },
+      { id: 3, name: 'Item 3', available: true },
+    ]
+
+    expect(result.length).toBe(2)
+    expect(result).toEqual(expected)
+  })
+})
