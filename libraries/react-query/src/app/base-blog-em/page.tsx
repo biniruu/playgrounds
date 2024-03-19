65502b4272be99a3921f8edb0eef517536b11a40
biniruu <biniruu@gmail.com>
2024-03-19 22:08:44 +0900

    feat: create Posts page

---
+++ libraries/react-query/src/app/base-blog-em/page.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+
+import { fetchPosts } from './api'
+import PostDetail from './postDetail'
+import styles from './posts.module.css'
+
+import type { Post } from 'types'
+
+const maxPostPage = 10
+
+const initValue = {
+  id: 0,
+  title: '',
+  body: '',
+  userId: 0,
+}
+
+function Posts() {
+  const [currentPage, setCurrentPage] = useState(0)
+  const [selectedPost, setSelectedPost] = useState<Post>(initValue)
+
+  const options = {
+    queryKey: ['posts', currentPage],
+    queryFn: () => fetchPosts(currentPage),
+  }
+
+  const { data } = useQuery(options)
+
+  if (!data?.length) {
+    return <div>🙀</div>
+  }
+
+  return (
+    <>
+      <ul>
+        {data.map(post => {
+          const { id, title } = post
+
+          return (
+            // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/click-events-have-key-events
+            <li key={id} className={styles['post-title']} onClick={() => setSelectedPost(post)}>
+              {title}
+            </li>
+          )
+        })}
+      </ul>
+      <div className={styles['pages']}>
+        <button disabled onClick={() => {}}>
+          Previous page
+        </button>
+        <span>Page {currentPage + 1}</span>
+        <button disabled onClick={() => {}}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  )
+}
+
+export default Posts
