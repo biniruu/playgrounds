ad4d942cd6d2b0817e8412a1dbe0c7c60d21bd39
biniruu <biniruu@gmail.com>
2024-03-19 21:42:46 +0900

    feat: update fetchPosts api

--- libraries/react-query/src/app/base-blog-em/api.ts
+++ libraries/react-query/src/app/base-blog-em/api.ts
@@ -1,29 +1,34 @@
-interface Post {
+export interface Post {
   id: number
   title: string
   body: string
   userId: number
 }
 
-export async function fetchPosts(pageNum = 1) {
+const fetchPosts = async (pageNum = 1) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`)
+  if (!response.ok) {
+    throw new Error('Network response was not ok')
+  }
   return response.json() as Promise<Post[]>
 }
 
-export async function fetchComments(postId: string) {
+const fetchComments = async (postId: string) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
   return response.json()
 }
 
-export async function deletePost(postId: string) {
+const deletePost = async (postId: string) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, { method: 'DELETE' })
   return response.json()
 }
 
-export async function updatePost(postId: string) {
+const updatePost = async (postId: string) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
     method: 'PATCH',
     body: JSON.stringify({ title: 'REACT QUERY FOREVER!!!!' }),
   })
   return response.json()
 }
+
+export { deletePost, fetchComments, fetchPosts, updatePost }
