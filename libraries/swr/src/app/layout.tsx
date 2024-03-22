edc7b309152f97115f70e25052a559697af83fef
biniruu <biniruu@gmail.com>
2024-03-22 17:18:24 +0900

    feat: implement swr provider

--- libraries/swr/src/app/layout.tsx
+++ libraries/swr/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
+
+import SWRProvider from 'providers/swrProvider'
 import './globals.css'
 
 // eslint-disable-next-line new-cap
@@ -18,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SWRProvider>{children}</SWRProvider>
+      </body>
     </html>
   )
 }
