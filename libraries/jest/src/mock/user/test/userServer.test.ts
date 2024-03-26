0e25d04a76ac066c7d35b1d0a1f7871cfd1f8e47
biniruu <biniruu@gmail.com>
2024-03-27 02:35:56 +0900

    test: correct login test

--- libraries/jest/src/mock/user/test/userServer.test.ts
+++ libraries/jest/src/mock/user/test/userServer.test.ts
@@ -1,15 +1,30 @@
-import { UserClient } from '@mock/user/stubUserClient'
+import { UserClient } from '@mock/user/userClient'
 import { UserServer } from '@mock/user/userServer'
+
+jest.mock('@mock/user/userClient')
 
 describe('Authenticate user', () => {
   let userServer: UserServer<UserClient>
+  const login = jest.fn(async () => 'success')
+  ;(UserClient as jest.Mock).mockImplementation(() => {
+    return { login }
+  })
 
   beforeEach(() => {
     userServer = new UserServer<UserClient>(new UserClient())
   })
 
-  test('should return boolean when authenticated user login', async () => {
+  test('should log in successfully when try to authenticate user login', async () => {
     await userServer.login('id', 'password')
+
+    expect(login).toHaveBeenCalled()
     expect(userServer.isLoggedIn).toBe(true)
+  })
+
+  test('should not execute login() when already logged in', async () => {
+    await userServer.login('id', 'password')
+    await userServer.login('id', 'password')
+
+    expect(login).toHaveBeenCalledTimes(1)
   })
 })
