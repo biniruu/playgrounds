a8cf82e1af1e11fc7845a134a9cd2b91f7e9cbfb
biniruu <biniruu@gmail.com>
2024-03-27 02:35:25 +0900

    test: remove stubUserClient.ts

--- libraries/jest/src/mock/user/stubUserClient.ts
+++
@@ -1,8 +0,0 @@
-class UserClient {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  login(id: string, password: string) {
-    return new Promise(res => res(true))
-  }
-}
-
-export { UserClient }
