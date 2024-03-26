1d6dc5c6abc922d97fcb865c847eb810aa631f66
biniruu <biniruu@gmail.com>
2024-03-27 02:33:50 +0900

    test: add return type to login function

--- libraries/jest/src/mock/user/userClient.ts
+++ libraries/jest/src/mock/user/userClient.ts
@@ -1,5 +1,5 @@
 class UserClient {
-  login(id: string, password: string) {
+  login(id: string, password: string): Promise<'success' | 'fail'> {
     return fetch(`http://example.com/login/${id}+${password}`).then(response => response.json())
   }
 }
