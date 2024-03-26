6206b2222cb81bee6a0debb8dda79fb82bef28b1
biniruu <biniruu@gmail.com>
2024-03-27 01:39:43 +0900

    test: create userClient.ts

---
+++ libraries/jest/src/mock/user/userClient.ts
@@ -0,0 +1,7 @@
+class UserClient {
+  login(id: string, password: string) {
+    return fetch(`http://example.com/login/${id}+${password}`).then(response => response.json())
+  }
+}
+
+export { UserClient }
