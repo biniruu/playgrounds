8198240ff8e4279b28c20e10bcbae15604091654
biniruu <biniruu@gmail.com>
2024-05-27 21:34:03 +0900

    feat: remove decorator folder

--- languages/typescript/src/index.ts
+++ languages/typescript/src/index.ts
@@ -1,30 +1,7 @@
-/* eslint-disable no-console */
 import { readFile } from 'fs'
 import type { IncomingMessage, ServerResponse } from 'http'
 import { createServer } from 'http'
 import { join } from 'path'
-
-// import Person2 from './decorator/decorator-factory'
-// import Person1 from './decorator/first-class-decorator'
-// import Person3 from './decorator/more-useful-decorator'
-// import Product from './decorator/property-decorator'
-
-// decorator
-// {
-//   const person1 = new Person1()
-//   const person2 = new Person2()
-//   const person3 = new Person3()
-//   const product = new Product('Product', 30)
-
-//   console.log('🚀 ~ person1:', person1)
-//   console.log('===============================================================================')
-//   console.log('🚀 ~ person2:', person2)
-//   console.log('===============================================================================')
-//   console.log('🚀 ~ person3:', person3)
-//   console.log('===============================================================================')
-//   product.price = 20
-//   console.log('🚀 ~ product:', product.price)
-// }
 
 const port = 3000
 
@@ -42,4 +19,5 @@ const server = (req: IncomingMessage, res: ServerResponse) => {
   res.end('Page not found')
 }
 
+// eslint-disable-next-line no-console
 createServer(server).listen(port, () => console.log(`Server running on port ${port}`))
